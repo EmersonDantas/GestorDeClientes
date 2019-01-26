fee7d44d10d7f0e4516e36e13eedf6b5974e579b
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-01-26 20:12:55 -0300

    Login implementado

--- clientes/views.py
+++ clientes/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
 from .forms import PersonForm
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def persons_list(request):
     persons = Person.objects.all()
     return render(request, 'person.html', {'persons':persons})
 
+@login_required
 def person_new(request):
     form = PersonForm(request.POST or None, request.FILES or None)
 
@@ -15,7 +18,7 @@ def person_new(request):
         return redirect('persons_list')
     return render(request, 'person_form.html', {'form':form})
 
-
+@login_required
 def person_update(request, id):
     person = get_object_or_404(Person, pk=id)
     form = PersonForm(request.POST or None, request.FILES or None, instance=person)
@@ -26,6 +29,7 @@ def person_update(request, id):
 
     return render(request, 'person_update.html', {'form':form})
 
+@login_required
 def person_delete(request, id):
     person = get_object_or_404(Person, pk=id)
     form = PersonForm(request.POST or None, request.FILES or None, instance=person)
